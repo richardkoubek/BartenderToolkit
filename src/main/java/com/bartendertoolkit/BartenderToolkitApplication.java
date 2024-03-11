5b71748f0fe8e406d5c04cd98fd5f344ba65e63a
Richard Koubek <koubek.richard@gmail.com>
2024-03-11 18:59:48 +0100

    innitial project file

---
+++ src/main/java/com/bartendertoolkit/BartenderToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.bartendertoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartenderToolkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BartenderToolkitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bartendertoolkit/BartenderToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartendertoolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartenderToolkitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
