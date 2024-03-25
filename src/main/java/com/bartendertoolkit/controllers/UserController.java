29beebe68ae012024efa01cad8e94694cad55156
Richard Koubek <koubek.richard@gmail.com>
2024-03-25 20:19:21 +0100

    register endpoint and html file

--- src/main/java/com/bartendertoolkit/controllers/UserController.java
+++ src/main/java/com/bartendertoolkit/controllers/UserController.java
@@ -1,13 +1,25 @@
 package com.bartendertoolkit.controllers;
 
+import com.bartendertoolkit.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping()
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
     @GetMapping("/home")
     public String home(){
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String registerForm(){
+
+        return "registerUser";
     }
 }
