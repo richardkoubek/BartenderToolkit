4a57429fe6dff2620e6993edb9d3c535833d0f2a
Richard Koubek <koubek.richard@gmail.com>
2024-03-26 11:05:37 +0100

    tips calculator endpoint and html

---
+++ src/main/java/com/bartendertoolkit/controllers/ToolsController.java
@@ -0,0 +1,18 @@
+package com.bartendertoolkit.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/tools")
+public class ToolsController {
+
+    @GetMapping("/tipscalculator")
+    public String tipsCalculator(){
+
+        return "tipsCalculator";
+    }
+}
