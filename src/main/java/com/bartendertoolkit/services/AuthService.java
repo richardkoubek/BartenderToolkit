38a63b3acb13971eb95950f9761c9ea896a3320f
Richard Koubek <koubek.richard@gmail.com>
2024-05-07 21:21:13 +0200

    authService static fields added

--- src/main/java/com/bartendertoolkit/services/AuthService.java
+++ src/main/java/com/bartendertoolkit/services/AuthService.java
@@ -1,7 +1,11 @@
 package com.bartendertoolkit.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    private static final int AUTH_COOKIE_TTL = 30 * 24 * 60 * 60;
+    private static final int KEY_LENGTH = 32;
+    
 }
