6a52d5aac5efab2e315c1b4cbe925c6cbfc19da0
Richard Koubek <koubek.richard@gmail.com>
2024-05-07 21:47:27 +0200

    UserDetails fromClaims function added

--- src/main/java/com/bartendertoolkit/services/UserDetailsImpl.java
+++ src/main/java/com/bartendertoolkit/services/UserDetailsImpl.java
@@ -1,11 +1,33 @@
 package com.bartendertoolkit.services;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import io.jsonwebtoken.Claims;
 
 import java.util.Collection;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
+
+    private Long id;
+    private String email;
+
+    public static UserDetailsImpl fromClaims(Claims claims) {
+        long id = Long.parseLong(claims.getId());
+
+        return UserDetailsImpl.builder()
+                .id(id)
+                .email(claims.getSubject())
+                .build();
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
