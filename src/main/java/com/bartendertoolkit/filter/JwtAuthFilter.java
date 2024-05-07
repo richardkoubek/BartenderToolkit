0e7f4f88db8275ccb54f1e846301dd07eafb5e28
Richard Koubek <koubek.richard@gmail.com>
2024-05-07 20:51:36 +0200

    JwtCookie updated

--- src/main/java/com/bartendertoolkit/filter/JwtAuthFilter.java
+++ src/main/java/com/bartendertoolkit/filter/JwtAuthFilter.java
@@ -6,11 +6,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
+    @Value("${authCookieName}")
+    private String jwtCookie;
     private AuthService authService;
     private UserService userService;
     public JwtAuthFilter(AuthService authService, UserService userService) {
