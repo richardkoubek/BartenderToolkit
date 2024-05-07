eed3509fef8d47013f312f34608f02e9999fd385
Richard Koubek <koubek.richard@gmail.com>
2024-05-07 20:58:34 +0200

    JwtAuthFilter doFilter function updated

--- src/main/java/com/bartendertoolkit/filter/JwtAuthFilter.java
+++ src/main/java/com/bartendertoolkit/filter/JwtAuthFilter.java
@@ -2,14 +2,18 @@ package com.bartendertoolkit.filter;
 
 import com.bartendertoolkit.services.AuthService;
 import com.bartendertoolkit.services.UserService;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
     @Value("${authCookieName}")
@@ -21,7 +25,26 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         this.userService = userService;
     }
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        Claims claims = null;
+
+        if (cookies != null){
+            Optional<Cookie> cookie = Arrays.stream(cookies)
+                    .filter(name -> name.getName().equals(jwtCookie))
+                    .findFirst();
+
+            if (cookie.isPresent()){
+                String token = cookie.get().getValue();
+                if(authService.validateToken(token)) {
+                    claims = authService.getAllClaimsFromToken(token);
+                }
+            }
+        }
 
     }
 }
